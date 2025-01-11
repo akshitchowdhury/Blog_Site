aa919a0ea02dbe1ae45a2e4089eb505d166f5c95
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-11 16:31:37 +0530

    blogInduce

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './component/Home/Home'
 
 function App() {
   
 
   return (
     <>
-    <p className='text-center text-3xl'>Hello</p>
+    <Home/>
     </>
   )
 }
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
+  const [blogList,setBlogList] = useState([]);
+  const fetchBlogs = async()=>{
+    const response = await fetch('http://localhost:3000/api/blogs');
+    const data = await response.json();
+    setBlogList(data); 
+  }
+
+  useEffect(()=>{
+    fetchBlogs();
+  },[])
   return (
     <div>
-      This is Home
+      {
+        blogList.map((blog)=>(
+          <>
+            <div key={blog._id}>
+              <h1>{blog.title}</h1>
+              <h2>{blog.content}</h2>
+              </div>
+          </>
+        ))
+      }
     </div>
   )
 }
